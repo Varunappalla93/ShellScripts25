8c7d0c30911b251493505d0f551bed1283387974
Varunappalla93 <an.varun1993@gmail.com>
2025-02-11 12:04:31 +0530

    Second Script

---
+++ variables_2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Ramesh :: Hello Suresh, gd mrg"
+echo "Suresh :: Hello Ramesh, v gd mrg"
+
+echo "Ramesh :: How are you Suresh"
+echo "Suresh :: am good Ramesh, how are you"
+
+
