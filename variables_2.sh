e5ce834099b638225fe332e5405e3db5f953f5aa
Varunappalla93 <an.varun1993@gmail.com>
2025-02-11 12:36:51 +0530

    Second Script

--- variables_2.sh
+++ variables_2.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-echo "Ramesh :: Hello Suresh, gd mrg"
-echo "Suresh :: Hello Ramesh, v gd mrg"
+name1="Ramesh"
+name2="Suresh"
 
-echo "Ramesh :: How are you Suresh"
-echo "Suresh :: am good Ramesh, how are you"
+echo "$name1 :: Hello $name2, gd mrg"
+echo "$name2 :: Hello $name1, v gd mrg"
 
-
+echo "$name1 :: How are you $name2"
+echo "$name2 :: am good $name1, how are you"
