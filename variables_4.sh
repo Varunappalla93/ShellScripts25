f32d476a2429543ff3ab5beddcd7d314a3a7d3b0
Varunappalla93 <an.varun1993@gmail.com>
2025-02-11 12:49:12 +0530

    Fourth Script

---
+++ variables_4.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+name1=$1
+name2=$2
+
+echo "$name1 :: Hello $name2, gd mrg"
+echo "$name2 :: Hello $name1, v gd mrg"
+
+echo "$name1 :: How are you $name2"
+echo "$name2 :: am good $name1, how are you"
