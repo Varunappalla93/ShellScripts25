dea077636ae10085a5fe648952a25c251b8401c4
Varunappalla93 <an.varun1993@gmail.com>
2025-02-11 13:54:10 +0530

    Fifth Script

--- variables_5.sh
+++ variables_5.sh
@@ -2,7 +2,7 @@
 
 echo "Please enter your username"
 
-read USERNAME
-
-echo "username is $username"
+read USERNAME  #value entered will be attached to username variable
+  
+echo "username is $USERNAME"
 
