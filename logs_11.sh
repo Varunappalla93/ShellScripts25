6a9e99759d5cc7a2d004d6fc6a5cd456d0fb0382
Varunappalla93 <an.varun1993@gmail.com>
2025-02-12 20:37:22 +0530

    Eleven Script

---
+++ logs_11.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+VALIDATE()
+{
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 is failed"
+        exit 1
+    else
+        echo "$2 is success"
+fi
+}
+
+
+ID=$(id -u) 
+echo "Sdript name : $0"
+
+
+if [ $ID -ne 0 ]
+then
+    echo "Error, pls try with root user"
+    exit 1 # non-zero
+else
+   echo "you are root user"
+fi
+
+
+yum install mysql -y
+# passing args in function
+VALIDATE $? "Installing mysql"
+
+yum install git -y
+VALIDATE $? "Installing git"
