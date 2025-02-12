f915e098f363defd3b0476f2e06d9eff314e9da0
Varunappalla93 <an.varun1993@gmail.com>
2025-02-12 16:07:20 +0530

    Tenth Script

--- functions_10.sh
+++ functions_10.sh
@@ -1,4 +1,31 @@
 #!/bin/bash
 
+VALIDATE()
+{
+    if [ $? -ne 0 ]
+    then
+        echo "installing software failed"
+        exit 1
+    else
+        echo "installing software success"
+fi
+}
 
 
+
+ID=$(id -u) 
+
+if [ $ID -ne 0 ]
+then
+    echo "Error, pls try with root user"
+    exit 1 # non-zero
+else
+   echo "you are root user"
+fi
+
+yum install mysql -y
+VALIDATE()
+
+
+yum install git -y
+VALIDATE()
