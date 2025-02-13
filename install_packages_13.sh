403e874036bf6390ddae9102ff7ce64f19f35713
Varunappalla93 <an.varun1993@gmail.com>
2025-02-13 14:46:28 +0530

    Thirteen  Script

--- install_packages_13.sh
+++ install_packages_13.sh
@@ -6,6 +6,24 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "Script started executing at $TIMESTAMP" &>>$LOGFILE
+
+
+VALIDATE()
+{
+if [ $1 -ne 0 ]
+    then
+        echo -e "ERROR:: $2 is $R failed $N"
+        exit 1
+    else
+        echo -e "$2 is $G successful $N"
+fi
+}
+
+
 if [ $ID -ne 0 ]
 then
     echo -e "$R Error, pls try with root user $N"
@@ -15,4 +33,14 @@ else
 fi
 
 
-echo "All args passed : $@"
+# echo "All args passed : $@" - git mysql postfix
+for package in $@
+do
+yum list installed $package
+if [ $? -ne 0 ]
+then
+    yum install $package -y
+    VALIDATE $? "Installing of $package"
+else
+    echo -e "$package is already installed..$Y SKIPPING $N"
+done
