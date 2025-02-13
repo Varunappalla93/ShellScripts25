39745972da3666b4b4703bcc6419db91bd2b990f
Varunappalla93 <an.varun1993@gmail.com>
2025-02-13 14:20:22 +0530

    Thirteen  Script

--- install_packages_13.sh
+++ install_packages_13.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+ID=$(id -u) 
 
+if [ $ID -ne 0 ]
+then
+    echo -e "$R Error, pls try with root user $N"
+    exit 1 # non-zero
+else
+   echo "you are root user"
+fi
+
+
+echo "All args passed : $@"
