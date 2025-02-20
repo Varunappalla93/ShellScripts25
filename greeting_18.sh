3e9fbc025d726430187b30e10a436d4a430798c8
Varunappalla93 <an.varun1993@gmail.com>
2025-02-20 15:53:56 +0530

    optargs

--- greeting_18.sh
+++ greeting_18.sh
@@ -6,7 +6,7 @@ wishes=""
 
 USAGE()
 {
-    echo "USAGE::$(basename $0) -n <name> -w <wishes>"
+    echo "USAGE:: $(basename $0) -n <name> -w <wishes>"
     echo "Options::"
     echo "-n, specify the name (mandatory)"
     echo "-w, specify the wishes (eg: gd mrg)"
@@ -14,11 +14,11 @@ USAGE()
 }
 
 
-while getopts ":n:w:h" opt;do
+while getopts ":n:w:h" opt; do
     case $opt in 
         n) name="$OPTARG";;
         w) wishes="$OPTARG";;
-        h|*) USAGE;exit;;
+        h|*) USAGE; exit;;
     esac
 done
 
