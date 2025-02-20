bc20d7a322c9358e79a9343ba38111fad02fc54d
Varunappalla93 <an.varun1993@gmail.com>
2025-02-20 15:49:48 +0530

    optargs

---
+++ greeting_18.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+name=""
+wishes=""
+
+USAGE()
+{
+    echo "USAGE::$(basename $0) -n <name> -w <wishes>"
+    echo "Options::"
+    echo "-n, specify the name (mandatory)"
+    echo "-w, specify the wishes (eg: gd mrg)"
+    echo "-h, display help and exit"
+}
+
+
+while getopts ":n:w:h" opt;do
+    case $opt in 
+        n) name="$OPTARG";;
+        w) wishes="$OPTARG"
+        h|*) USAGE;exit;;
+    esac
+done
+
